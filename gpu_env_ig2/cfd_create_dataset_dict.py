e8cce79a4ef20675afa01b169245aa9f28a847b7
dedpool-glitch <nalinrajput2001@gmail.com>
2025-06-27 16:07:47 +0530

    all local changes pushed

---
+++ gpu_env_ig2/cfd_create_dataset_dict.py
@@ -0,0 +1,30 @@
+import os
+
+def create_dataset_dict(source_folder):
+    dataset = {}
+    emotion_map = {'N': 0, 'A': 1, 'F': 2, 'HC': 3, 'HO': 3}
+
+    for folder in os.listdir(source_folder):
+        folder_path = os.path.join(source_folder, folder)
+
+        if not os.path.isdir(folder_path):
+            continue
+
+        for file_name in os.listdir(folder_path):
+            if file_name.endswith('.jpg'):
+                parts = file_name.split('-')
+                if len(parts) < 5:
+                    continue 
+
+                emotion_key = parts[4].split(".")[0] 
+                label = emotion_map.get(emotion_key, None)
+
+                if label is not None:
+                    dataset[folder_path+file_name] = label
+
+    return dataset
+
+source_directory_path = "D:\Integrated_gap_gradients\ig2_CNN\gpu_env_ig2\CFD"
+dataset_dict = create_dataset_dict(source_directory_path)
+
+print(f"Total Images: {len(dataset_dict)}")
